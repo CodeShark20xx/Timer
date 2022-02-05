41821dd8e5c49e2afd2bd21d53c54b42c9cf6206
Joshua Whipple <jwhipple@greencourt.com>
2022-02-05 12:28:53 -0500

    Functioning Timer

--- timer.js
+++ timer.js
@@ -1,35 +1,49 @@
-let hours = 0;
-let minutes = 1;
-let seconds = 11;
-
-const timeFormatter = (hours, minutes, seconds) => {
-    let formattedTime = '';
-    formattedTime = addLeadingZero(hours);
-    formattedTime += ":" + addLeadingZero(minutes);
-    formattedTime += ":" + addLeadingZero(seconds);
-    return formattedTime;
-}
 
 const addLeadingZero = (num) => {
     return (num < 10) ? `0${num}` : num.toString();
 }
 
-const stopCountDown = (timer) => {
-    clearInterval(timer);
+const stopCountDown = (timerID) => {
+    clearInterval(timerID);
 }
 
+const displayTimeUnit = (timeUnit, value) => {
+    document.getElementById('display' + timeUnit).innerText = addLeadingZero(value);
+}
 
-let timerHandler = (hours, minutes, seconds) => {
+const renderTime = (hours, minutes, seconds) => {
+    displayTimeUnit('Hours', hours);
+    displayTimeUnit('Minutes', minutes);
+    displayTimeUnit('Seconds', seconds);
+}
+
+const resetTo59 = (num) => {
+    return num >= 0 ? num : 59;
+}
+
+const timerHandler = () => {
+    let hours = Number.parseInt(document.getElementById('displayHours').innerText);
+    let minutes = Number.parseInt(document.getElementById('displayMinutes').innerText);
+    let seconds = Number.parseInt(document.getElementById('displaySeconds').innerText);
+
     if(hours !== 0 || minutes !== 0 || seconds !== 0){
-        console.clear;
-        console.log(`Tick: ${hours} ${minutes} ${seconds}`);
-        seconds--;
-    } else {
-        console.log('Time Up!');
+        seconds = resetTo59(seconds - 1);
+        console.log('Tick...');
+    }
+    if((hours !== 0 || minutes !== 0) && seconds === 59){
+        minutes = resetTo59(minutes - 1);
+    }
+    if((hours !== 0 && minutes === 59) && seconds === 59){
+        hours = resetTo59(hours - 1);
+    }
+
+    renderTime(hours, minutes, seconds);
+
+    if(hours === 0 && minutes === 0 && seconds === 0){
         stopCountDown(intervalID);
     }
 }
 
 let intervalID = setInterval(() => {
-    timerHandler(hours, minutes, seconds)
+    timerHandler();
 }, 1000);
